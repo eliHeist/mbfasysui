b32294dc1d2313d57b89e9dabace1fefc422b9c6
Elijah <eliyang256@outlook.com>
2024-01-25 11:22:22 +0300

    adapter node

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from '@sveltejs/adapter-auto'; 
+import adapter from '@sveltejs/adapter-node'; 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import preprocessor from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
